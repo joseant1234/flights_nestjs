70b15d233d04eeb425414867e0729cf68963def1
Jose Tafur <j-osea-nt@hotmail.com>
2020-01-22 22:12:53 -0500

    adding the scripts

---
+++ dropDatabase.ts
@@ -0,0 +1,16 @@
+import * as execSh from 'exec-sh';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const host = 'localhost';
+const port = process.env.DB_PORT;
+const databasse = process.env.DB_NAME;
+
+const command = `psql "user=${username} password=${password} host=${host} port=${port} sslmode=require" -c "drop database ${databasse};" `
+execSh(command, {}, function(err){
+  if (err) {
+    console.log("Exit code: ", err.code);
+  }
+});
